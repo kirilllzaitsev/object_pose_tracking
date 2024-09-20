b574a5ca0bdf43b7d361c9f5e9ceb150b117a440
kirilllzaitsev <kzaitsev88@gmail.com>
2024-09-20 23:21:46 +0200

    ds load seq

--- pose_tracking/dataset/bop.py
+++ pose_tracking/dataset/bop.py
@@ -1,8 +1,18 @@
 import os
+from collections import defaultdict
 
+import numpy as np
+import torch
+from PIL import Image
 from pose_tracking.dataset.bop_loaders import load_cad, load_list_scene, load_metadata
 from torch.utils.data import Dataset
 from tqdm import tqdm
+
+from bop_toolkit_lib.dataset.bop_imagewise import (
+    io_load_masks,
+    load_bop_depth,
+    load_bop_rgb,
+)
 
 
 class BaseBOP(Dataset):
@@ -13,6 +23,7 @@ class BaseBOP(Dataset):
         rot_repr="rotation6d",
         cad_dir=None,
         do_load_cad=False,
+        seq_length=1,
     ):
         """
         Read a dataset in the BOP format.
@@ -36,6 +47,75 @@ class BaseBOP(Dataset):
             self.cad_dir = cad_dir
         else:
             self.cads = None
+
+        self.trajs = []
+        self.seq_length = seq_length
+        self.step_skip = 1
+        self.seq_start = 0
+        for scene_id, traj_objs in list(self.metadata.groupby("scene_id")):
+            traj_objs_grouped = traj_objs.groupby("frame_id")
+            num_frames = len(traj_objs_grouped)
+            print(f"{num_frames=}")
+            num_frames = len(traj_objs_grouped)
+            for frame_id, traj in traj_objs_grouped:
+                self.trajs.append((scene_id, frame_id, traj_objs))
+                # if num_frames >= seq_length:
+                #     for i in range(num_frames - seq_length + 1):
+                #         self.trajs.append((scene_id, i, traj))
+
+    def __len__(self):
+        return len(self.trajs)
+
+    def __getitem__(self, idx):
+        seq_start = self.seq_start
+        scene_id, seq_start, traj_objs = self.trajs[idx]
+        traj_objs.pose = traj_objs.pose.apply(
+            lambda x: torch.tensor(np.array(np.matrix(x, dtype=np.float32)).reshape(4, 4))
+        )
+        traj = [to[1] for to in traj_objs.groupby("frame_id")]
+        if seq_start is None:
+            seq_start = torch.randint(
+                0,
+                max(1, len(traj) + 1 - self.seq_length * self.step_skip),
+                (1,),
+            ).item()
+        assert self.step_skip > 0, f"{self.step_skip=}"
+        sample_traj_seq = defaultdict(list)
+
+        for ts in range(self.seq_length):
+            frame_idx = seq_start + ts * self.step_skip
+            sample = traj[frame_idx].to_dict(orient="list")
+            #         ['scene_id', 'frame_id', 'obj_id', 'idx_obj', 'pose', 'rgb_path',
+            #    'mask_path', 'mask_visib_path', 'depth_path', 'visib_fract', 'bbox_obj',
+            #    'bbox_visib', 'intrinsic']
+            masks = [Image.open(p) for p in sample["mask_path"]]
+            masks_visib = [Image.open(p) for p in sample["mask_visib_path"]]
+            depth = load_bop_depth(sample["depth_path"])
+            rgb = load_bop_rgb(sample["rgb_path"])
+
+            sample["mask"] = masks
+            sample["mask_visib"] = masks_visib
+            sample["depth"] = depth
+            sample["rgb"] = rgb
+
+            for k in [
+                "scene_id",
+                "frame_id",
+                "obj_id",
+                "idx_obj",
+                "pose",
+                "rgb_path",
+                "mask_path",
+                "mask_visib_path",
+                "depth_path",
+                "visib_fract",
+                "bbox_obj",
+                "bbox_visib",
+                "intrinsic",
+            ]:
+                sample_traj_seq[k].append(sample[k])
+
+        return sample_traj_seq
 
 
 if __name__ == "__main__":
