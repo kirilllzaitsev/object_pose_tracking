99777635336c302ca90053bad38ac4cd06b0ae42
kirilllzaitsev <kzaitsev88@gmail.com>
2024-09-27 22:47:03 +0200

    train ref

--- pose_tracking/train.py
+++ pose_tracking/train.py
@@ -1,20 +1,23 @@
 import argparse
 import os
+import pickle
+import shutil
 from collections import defaultdict
 from datetime import datetime
 from pathlib import Path
 
 import cv2
+import matplotlib
 import numpy as np
 import torch
 import torch.distributed as dist
 import torch.nn as nn
 import torch.optim as optim
-from loguru import logger
 from pose_tracking.callbacks import EarlyStopping
-from pose_tracking.config import DATA_DIR, PROJ_DIR, WORKSPACE_DIR
+from pose_tracking.config import DATA_DIR, PROJ_DIR, WORKSPACE_DIR, logger
 from pose_tracking.dataset.bop import BOPDataset
 from pose_tracking.losses import geodesic_loss
+from pose_tracking.utils.common import print_args
 from pose_tracking.utils.rotation_conversions import (
     matrix_to_quaternion,
     quaternion_to_matrix,
@@ -24,52 +27,80 @@ from torch.utils.data import DataLoader, DistributedSampler
 from torchvision import datasets, models, transforms
 from tqdm.auto import tqdm
 
+matplotlib.use("TKAgg")
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--epochs", type=int, default=100, help="Number of training epochs")
-    parser.add_argument("--validate_every", type=int, default=5, help="Validate every N epochs")
-    parser.add_argument("--save_every", type=int, default=10, help="Save model every N epochs")
-    parser.add_argument("--ddp", action="store_true", help="Use Distributed Data Parallel")
-    parser.add_argument("--local_rank", type=int, default=0, help="Local rank for distributed training")
-    parser.add_argument("--batch_size", type=int, default=2, help="Batch size for training")
-    parser.add_argument("--use_early_stopping", action="store_true", help="Use early stopping")
-    parser.add_argument("--do_overfit", action="store_true", help="Overfit setting")
+
+    train_args = parser.add_argument_group("Training arguments")
+    train_args.add_argument("--epochs", type=int, default=100, help="Number of training epochs")
+    train_args.add_argument("--validate_every", type=int, default=5, help="Validate every N epochs")
+    train_args.add_argument("--save_every", type=int, default=10, help="Save model every N epochs")
+    train_args.add_argument("--ddp", action="store_true", help="Use Distributed Data Parallel")
+    train_args.add_argument("--local_rank", type=int, default=0, help="Local rank for distributed training")
+    train_args.add_argument("--batch_size", type=int, default=2, help="Batch size for training")
+    train_args.add_argument("--use_early_stopping", action="store_true", help="Use early stopping")
+    train_args.add_argument("--do_overfit", action="store_true", help="Overfit setting")
+    train_args.add_argument("--do_debug", action="store_true", help="Debugging setting")
+
     return parser.parse_args()
 
 
+def postprocess_args(args):
+    if args.do_overfit:
+        args.dropout_prob = 0.0
+
+    return args
+
+
 class PoseCNN(nn.Module):
-    def __init__(self):
-        super(PoseCNN, self).__init__()
+    def __init__(self, dropout_prob=0.3, backbone_name="mobilenet_v3_large"):
+        super().__init__()
 
-        backbone = models.resnet50(pretrained=True)
+        if backbone_name == "resnet50":
+            backbone = models.resnet50(pretrained=True)
+            last_backbone_ch = 2048
+        elif backbone_name == "mobilenet_v3_large":
+            backbone = models.mobilenet_v3_large(pretrained=True)
+            last_backbone_ch = 960
+        else:
+            raise ValueError(f"Unknown backbone: {backbone_name}")
+        self.backbone_name = backbone_name
 
-        # self.modify_resnet50_first_conv(backbone)
+        self.do_modify_first_conv = True
+        self.do_modify_first_conv = False
+        if self.do_modify_first_conv:
+            self.modify_first_conv(backbone)
 
         modules = list(backbone.children())[:-2]
         self.features = nn.Sequential(*modules)
 
         self.translation_head = nn.Sequential(
-            nn.Conv2d(2048, 256, kernel_size=3, padding=1),
+            nn.Conv2d(last_backbone_ch, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.AdaptiveAvgPool2d(1),
             nn.Flatten(),
-            nn.Dropout(0.5),
+            nn.Dropout(dropout_prob),
             nn.Linear(256, 3),
         )
 
         self.rotation_head = nn.Sequential(
-            nn.Conv2d(2048, 256, kernel_size=3, padding=1),
+            nn.Conv2d(last_backbone_ch, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.AdaptiveAvgPool2d(1),
             nn.Flatten(),
-            nn.Dropout(0.5),
+            nn.Dropout(dropout_prob),
             nn.Linear(256, 4),
             nn.Tanh(),
         )
 
-    def modify_resnet50_first_conv(self, backbone):
-        original_conv = backbone.conv1
+    def modify_first_conv(self, backbone):
+        if self.backbone_name == "mobilenet_v3_large":
+            original_conv = backbone.features[0][0]
+        else:
+            original_conv = backbone.conv1
+
         new_conv = nn.Conv2d(
             in_channels=4,
             out_channels=original_conv.out_channels,
@@ -80,15 +111,17 @@ class PoseCNN(nn.Module):
         )
 
         with torch.no_grad():
-
             new_conv.weight[:, :3, :, :] = original_conv.weight
-
             new_conv.weight[:, 3:, :, :] = original_conv.weight.mean(dim=1, keepdim=True)
 
-        backbone.conv1 = new_conv
+        if self.backbone_name == "mobilenet_v3_large":
+            backbone.features[0][0] = new_conv
+        else:
+            backbone.conv1 = new_conv
 
     def forward(self, x, segm_mask):
-        # x = torch.cat([x, segm_mask], dim=1)
+        if self.do_modify_first_conv:
+            x = torch.cat([x, segm_mask], dim=1)
         features = self.features(x)
 
         trans_output = self.translation_head(features)
@@ -126,6 +159,8 @@ class SceneDataset(torch.utils.data.Dataset):
         mask = self.scene["mask"][idx][obj_idx]
         rgb_path = self.scene["rgb_path"][idx]
 
+        # rgb = (rgb * mask).float()
+
         if self.transform:
             rgb = self.transform(rgb)
             mask = self.transform(mask)
@@ -140,10 +175,18 @@ class SceneDataset(torch.utils.data.Dataset):
 
         return {"frame_id": frame_id, "rgb": rgb, "mask": mask, "r": r_quat, "t": t, "rgb_path": rgb_path}
 
+    def clone(self, idxs=None):
+        scene = self.scene
+        if idxs is not None:
+            scene = {k: [v[i] for i in idxs] for k, v in scene.items()}
+        return SceneDataset(scene, self.transform)
+
 
 def main():
     logger.info(f"PROJ_ROOT path is: {PROJ_DIR}")
     args = parse_args()
+    args = postprocess_args(args)
+    print_args(args)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -154,16 +197,16 @@ def main():
         device = torch.device("cuda", args.local_rank)
 
     model = PoseCNN().to(device)
+    logger.info(f"model.parameters={sum(p.numel() for p in model.parameters() if p.requires_grad)}")
 
     if args.ddp:
         model = DDP(model, device_ids=[args.local_rank], output_device=args.local_rank)
 
     criterion_trans = nn.MSELoss()
-    criterion_rot = nn.MSELoss()
     criterion_rot = geodesic_loss
 
-    optimizer = optim.Adam(model.parameters(), lr=5e-4)
-    lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=args.epochs // 4, gamma=0.5, verbose=False)
+    optimizer = optim.Adam(model.parameters(), lr=1e-4)
+    lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=args.epochs // 1, gamma=0.5, verbose=False)
 
     transform = transforms.Compose([])
 
@@ -187,13 +230,19 @@ def main():
     scene = full_ds[0]
     scene_len = len(scene["frame_id"])
     logger.info(f"Scene length: {scene_len}")
-    train_share = 0.2 if args.do_overfit else 0.8
+    train_share = 0.8
     train_len = int(train_share * scene_len)
     scene_train = {k: v[:train_len] for k, v in scene.items()}
     scene_val = {k: v[train_len:] for k, v in scene.items()}
 
     train_dataset = SceneDataset(scene=scene_train, transform=transform)
     val_dataset = SceneDataset(scene=scene_val, transform=transform)
+
+    if args.do_overfit:
+        train_dataset = train_dataset.clone([0])
+        val_dataset = train_dataset
+
+    logger.info(f"{len(train_dataset)=}")
 
     preds_base_dir = f"{WORKSPACE_DIR}/preds"
     now = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
@@ -220,12 +269,14 @@ def main():
         )
         val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, collate_fn=collate_fn)
 
-    for epoch in range(1, args.epochs + 1):
+    history = defaultdict(lambda: defaultdict(list))
+
+    for epoch in tqdm(range(1, args.epochs + 1), desc="Epochs"):
         model.train()
         if args.ddp:
             train_loader.sampler.set_epoch(epoch)
-        running_loss = 0.0
-        for batch_idx, batch in enumerate(train_loader):
+        running_losses = defaultdict(float)
+        for batch_idx, batch in enumerate(tqdm(train_loader, desc="Batches", leave=False)):
             images = batch["rgb"]
             seg_masks = batch["mask"].float()
             trans_labels = batch["t"]
@@ -245,9 +296,16 @@ def main():
             loss.backward()
             optimizer.step()
 
-            running_loss += loss.item()
-            if batch_idx % 10 == 0:
-                print(f"Epoch [{epoch}/{args.epochs}], Step [{batch_idx}/{len(train_loader)}], Loss: {loss.item():.4f}")
+            running_losses["loss"] += loss
+            running_losses["loss_rot"] += loss_rot
+            running_losses["loss_trans"] += loss_trans
+
+        logger.info(f"#### Epoch {epoch} ####")
+        for k, v in running_losses.items():
+            v /= len(train_loader)
+            running_loss = v.item()
+            logger.info(f"{k}: {running_loss:.4f}")
+            history["train"][k].append(running_loss)
 
         lr_scheduler.step()
         if epoch % args.save_every == 0:
@@ -274,14 +332,18 @@ def main():
                     loss = loss_trans + loss_rot
 
                     val_loss += loss.item() * images.size(0)
-            val_loss /= len(val_loader.dataset)
-            print(f"Validation Loss after Epoch {epoch}: {val_loss:.4f}")
+            val_loss /= len(val_loader)
+            logger.info(f"Validation Loss after Epoch {epoch}: {val_loss:.4f}")
 
             if args.use_early_stopping:
                 early_stopping(loss=val_loss)
                 if early_stopping.do_stop:
-                    print(f"Early stopping on epoch {epoch}")
+                    logger.warning(f"Early stopping on epoch {epoch}")
                     break
+
+    if args.do_debug:
+        shutil.rmtree(preds_dir)
+        return
 
     save_model(model, model_path)
 
@@ -318,7 +380,7 @@ def main():
             rgb_path = str(rgb_path)
             cv2.imwrite(rgb_path, rgb)
 
-    print(f"saved to {preds_dir=}")
+    logger.info(f"saved to {preds_dir=} {preds_dir.name}")
 
     if args.ddp:
         dist.destroy_process_group()
