33a017a0b5827f2cc273f653f17cebdbaab51ffe
kirilllzaitsev <kzaitsev88@gmail.com>
2024-09-28 09:46:03 +0200

    log tweak

--- pose_tracking/config.py
+++ pose_tracking/config.py
@@ -1,7 +1,9 @@
+import functools
+import sys
+import traceback
 from pathlib import Path
 
 from dotenv import load_dotenv
-from loguru import logger
 
 load_dotenv()
 
@@ -16,10 +18,75 @@ YCBINEOAT_SCENE_DIR = DATA_DIR / "ycbineoat"
 
 YCB_MESHES_DIR = f"{DATA_DIR}/ycb/models_bt"
 
-try:
-    from tqdm import tqdm
 
-    logger.remove(0)
-    logger.add(lambda msg: tqdm.write(msg, end=""), colorize=True)
-except ModuleNotFoundError:
-    pass
+def prepare_logger(logpath=None, level="INFO"):
+    from loguru import logger
+
+    # Define handlers
+    common_cfg = {
+        "format": "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | {level} | {file}:{line} | <level>{message}</level>",
+        "level": level,
+        "colorize": True,
+    }
+    handlers = [
+        {
+            "sink": sys.stdout,
+            **common_cfg,
+        }
+    ]
+
+    if logpath:
+        handlers.append(
+            {
+                "sink": logpath,
+                **common_cfg,
+            }
+        )
+
+    try:
+        from tqdm import tqdm
+
+        def tqdm_sink(message):
+            tqdm.write(message, end="")
+
+        handler_tqdm = {
+            "sink": tqdm_sink,
+            **common_cfg,
+        }
+        handlers[0] = handler_tqdm
+    except ModuleNotFoundError:
+        logger.debug("tqdm not found. Skipping tqdm integration.")
+
+    # Configure logger with the defined handlers
+    logger.remove()
+    logger.configure(handlers=handlers)
+    sys.excepthook = functools.partial(log_exception, logger)
+    return logger
+
+
+def log_exception(logger, *args):
+    if len(args) == 1:
+        e = args[0]
+        etype, value, tb = type(e), e, e.__traceback__
+    elif len(args) == 3:
+        etype, value, tb = args
+    else:
+        logger.error(
+            "Not able to log exception. Wrong number of arguments given. Should either receive 1 argument "
+            "- an exception, or 3 arguments: exc type, exc value and traceback"
+        )
+        return
+
+    tb_msg = "".join(traceback.format_exception(etype, value, tb))
+    logger.exception(f"{tb_msg}")
+
+
+logger = prepare_logger()
+
+if __name__ == "__main__":
+    logger.info(f"PROJ_DIR: {PROJ_DIR}")
+    logger.warning(f"PROJ_DIR: {PROJ_DIR}")
+    logger.error(f"PROJ_DIR: {PROJ_DIR}")
+    logger.critical(f"PROJ_DIR: {PROJ_DIR}")
+    logger.debug(f"PROJ_DIR: {PROJ_DIR}")
+    raise ValueError("This is a test exception")
