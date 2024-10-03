75f4a50d553439f3f9abee12e621bcd573eec58d
kirilllzaitsev <kzaitsev88@gmail.com>
2024-10-03 23:39:21 +0200

    kpt_utils add get_matches

--- pose_tracking/utils/kpt_utils.py
+++ pose_tracking/utils/kpt_utils.py
@@ -38,3 +38,31 @@ def get_pose_from_matches(mkpts0, mkpts1, camera_matrix):
 
     _, R, t, _ = cv2.recoverPose(E, inliers1, inliers2, cameraMatrix=camera_matrix_em)
     return {"R": R, "t": t}
+def get_matches(image0_rgb, image1_rgb, extractor, matcher):
+
+    # load each image as a torch.Tensor on GPU with shape (3,H,W), normalized in [0,1]
+    image0 = image0_rgb.cuda()
+    image1 = image1_rgb.cuda()
+
+    times = []
+    for _ in range(1):
+        start = time.time()
+        # extract local features
+        feats0 = extractor.extract(image0)  # auto-resize the image, disable with resize=None
+        feats1 = extractor.extract(image1)
+
+        # match the features
+        matches01 = matcher({"image0": feats0, "image1": feats1})
+        times.append(time.time() - start)
+
+    feats0, feats1, matches01 = [rbd(x) for x in [feats0, feats1, matches01]]  # remove batch dimension
+    matches = matches01["matches"]  # indices with shape (K,2)
+    mkpts0 = feats0["keypoints"][matches[..., 0]].cpu()  # coordinates in image #0, shape (K,2)
+    mkpts1 = feats1["keypoints"][matches[..., 1]].cpu()  # coordinates in image #1, shape (K,2)
+
+    return {
+        "mkpts0": mkpts0,
+        "mkpts1": mkpts1,
+        "scores": matches01["scores"],
+        "times": times,
+    }
