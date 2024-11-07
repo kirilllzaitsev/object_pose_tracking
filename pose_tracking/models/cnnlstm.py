21fb0f77abafc188852bd42d10d29bcb43a79b57
kirilllzaitsev <kzaitsev88@gmail.com>
2024-11-07 13:03:07 +0100

    m: add lstmcell-jit

---
+++ pose_tracking/models/cnnlstm.py
@@ -0,0 +1,38 @@
+import torch
+from torch import jit
+from torch.nn import Parameter
+
+
+class LSTMCell(jit.ScriptModule):
+    def __init__(self, input_size, hidden_size):
+        super(LSTMCell, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.weight_ih = Parameter(torch.randn(4 * hidden_size, input_size))
+        self.weight_hh = Parameter(torch.randn(4 * hidden_size, hidden_size))
+        self.bias_ih = Parameter(torch.randn(4 * hidden_size))
+        self.bias_hh = Parameter(torch.randn(4 * hidden_size))
+
+    @jit.script_method
+    def forward(self, input, hx, cx):
+        gates = torch.mm(input, self.weight_ih.t()) + self.bias_ih + torch.mm(hx, self.weight_hh.t()) + self.bias_hh
+        ingate, forgetgate, cellgate, outgate = gates.chunk(4, 1)
+
+        ingate = torch.sigmoid(ingate)
+        forgetgate = torch.sigmoid(forgetgate)
+        cellgate = torch.tanh(cellgate)
+        outgate = torch.sigmoid(outgate)
+
+        cy = (forgetgate * cx) + (ingate * cellgate)
+        hy = outgate * torch.tanh(cy)
+
+        return hy, (hy, cy)
+
+
+if __name__ == "__main__":
+    model = LSTMCell(10, 20)
+    input = torch.randn(6, 10)
+    hx = torch.randn(6, 20)
+    cx = torch.randn(6, 20)
+    output = model(input, hx, cx)
+    print(output[0].shape)
