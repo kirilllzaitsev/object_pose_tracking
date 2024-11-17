f1be142f16d8809ff811ab1d80d491e68b0b5967
kirilllzaitsev <kzaitsev88@gmail.com>
2024-11-17 14:30:12 +0100

    d:load video with SequenceSampler

--- pose_tracking/dataset/video_ds.py
+++ pose_tracking/dataset/video_ds.py
@@ -1,5 +1,5 @@
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import DataLoader, Dataset, Sampler
 
 
 class VideoDataset(Dataset):
@@ -13,32 +13,23 @@ class VideoDataset(Dataset):
         num_samples: Number of times to sample a sequence from the video (length of the dataset)
     """
 
-    def __init__(self, ds, seq_len=10, seq_start=None, seq_step=1, num_samples=None):
+    def __init__(self, ds, seq_len=10, seq_start=None, seq_step=1, num_samples=None, num_workers=8):
         self.ds = ds
         self.seq_len = seq_len
         self.seq_start = seq_start
         self.seq_step = seq_step
         self.num_samples = len(ds) if num_samples is None else num_samples
+        self.num_workers = num_workers
 
     def __len__(self):
         return self.num_samples
 
     def __getitem__(self, idx):
+        sampler = SequenceSampler(len(self.ds), seq_len=self.seq_len, seq_step=self.seq_step, seq_start=self.seq_start)
+        dataloader = DataLoader(self.ds, batch_size=1, sampler=sampler, num_workers=self.num_workers)
         seq = []
-        timesteps = min(len(self.ds), self.seq_len) if self.seq_len is not None else len(self.ds)
-        seq_start = self.seq_start
-        if seq_start is None:
-            seq_start = torch.randint(
-                0,
-                max(1, len(self.ds) + 1 - timesteps * self.seq_step),
-                (1,),
-            ).item()
-        assert self.seq_step > 0, f"{self.seq_step=}"
-
-        for t in range(timesteps):
-            frame_idx = seq_start + t * self.seq_step
-            sample = self.ds[frame_idx]
-            seq.append(sample)
+        for batch in dataloader:
+            seq.append(batch)
         return seq
 
 
@@ -70,3 +61,37 @@ class MultiVideoDataset(Dataset):
     def __getitem__(self, idx):
         video_idx = torch.randint(0, len(self.video_datasets), (1,)).item()
         return self.video_datasets[video_idx][idx]
+
+
+class SequenceSampler(Sampler):
+    def __init__(self, dataset_len, seq_len=None, seq_step=1, seq_start=None):
+        self.dataset_len = dataset_len
+        self.seq_len = seq_len
+        self.seq_step = seq_step
+        self.seq_start = seq_start
+
+    def __iter__(self):
+        timesteps = min(self.dataset_len, self.seq_len) if self.seq_len is not None else self.dataset_len
+        seq_start = self.seq_start
+
+        if seq_start is None:
+            seq_start = torch.randint(
+                0,
+                max(1, self.dataset_len + 1 - timesteps * self.seq_step),
+                (1,),
+            ).item()
+
+        assert self.seq_step > 0, f"{self.seq_step=}"
+
+        frame_idxs = []
+        for t in range(timesteps):
+            frame_idx = seq_start + t * self.seq_step
+            if frame_idx < self.dataset_len:
+                frame_idxs.append(frame_idx)
+
+        return iter(frame_idxs)
+
+    def __len__(self):
+        if self.seq_len is not None:
+            return self.seq_len
+        return self.dataset_len
