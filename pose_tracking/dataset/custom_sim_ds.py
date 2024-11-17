320f17322ac1c4bd1ffd1a920760af402ef4bc99
kirilllzaitsev <kzaitsev88@gmail.com>
2024-11-17 22:52:53 +0100

    d:adjust custom sim to ycbi

--- pose_tracking/dataset/custom_sim_ds.py
+++ pose_tracking/dataset/custom_sim_ds.py
@@ -12,20 +12,23 @@
     └── *.png
 """
 
+import copy
 import glob
 import os
 from pathlib import Path
 
 import cv2
-import imageio
 import numpy as np
 import torch
 import trimesh
 from pose_tracking.config import logger
-from pose_tracking.dataset.ds_common import get_ds_sample
-from pose_tracking.utils.common import cast_to_numpy
-from pose_tracking.utils.geom import get_inv_pose
+from pose_tracking.dataset.ds_common import process_raw_sample
+from pose_tracking.utils.geom import backproj_depth, get_inv_pose
 from pose_tracking.utils.io import load_color, load_depth, load_mask, load_pose
+from pose_tracking.utils.pcl import (
+    downsample_pcl_via_subsampling,
+    downsample_pcl_via_voxels,
+)
 from pose_tracking.utils.rotation_conversions import quaternion_to_matrix
 from pose_tracking.utils.segm_utils import mask_erode
 from pose_tracking.utils.trimesh_utils import load_mesh
@@ -46,10 +49,20 @@ class CustomSimDataset(Dataset):
         cam_pose_path=None,
         do_remap_pose_from_isaac=False,
         do_erode_mask=False,
+        use_priv_info=False,
+        obj_name="cube",
+        convert_pose_to_quat=False,
+        num_mesh_pts=2000,
     ):
+        self.include_masks = include_masks
+        self.do_remap_pose_from_isaac = do_remap_pose_from_isaac
+        self.do_erode_mask = do_erode_mask
+        self.use_priv_info = use_priv_info
+        self.convert_pose_to_quat = convert_pose_to_quat
         self.root_dir = root_dir
         self.transforms = transforms
-        self.include_masks = include_masks
+        self.obj_name = obj_name
+
         self.color_files = sorted(glob.glob(f"{self.root_dir}/rgb/*.png"))
         self.K = np.loadtxt(f"{self.root_dir}/intrinsics.txt").reshape(3, 3)
         self.id_strs = []
@@ -91,7 +104,9 @@ class CustomSimDataset(Dataset):
         if mesh_path is not None:
             load_res = load_mesh(mesh_path)
             self.mesh = load_res["mesh"]
-            self.mesh_bbox = load_res["bbox"]
+            self.mesh_bbox = copy.deepcopy(np.asarray(load_res["bbox"]))
+            self.mesh_diameter = load_res["diameter"]
+            self.mesh_pts = torch.tensor(trimesh.sample.sample_surface(self.mesh, num_mesh_pts)[0]).float()
 
     def __len__(self):
         return len(self.color_files)
@@ -100,19 +115,9 @@ class CustomSimDataset(Dataset):
         path = self.color_files[idx]
         color = self.get_color(idx)
         depth_raw = self.get_depth(idx)
-
-        priv = None
-        if self.use_priv_info:
-            sample_no_occ = self.ds_no_occ[idx]
-            depth_no_occ = sample_no_occ["depth"]
-            mask = sample_no_occ["mask"]
-            obj_depth = (depth_no_occ).squeeze().numpy()
-            obj_depth[mask == 0] = 0
-            obj_depth_3d, _ = backproj_depth(obj_depth, intrinsics=self.K)
-            obj_depth_3d = downsample_pcl_via_voxels(obj_depth_3d, voxel_size=0.01)
-            priv = downsample_pcl_via_subsampling(obj_depth_3d, num_pts=256)
-
-        sample = {}
+        pose = load_pose(
+            Path(self.color_files[idx].replace("rgb/", "pose/")).parent / self.obj_name / f"{self.id_strs[idx]}.txt"
+        )
 
         priv = None
         if self.use_priv_info:
@@ -131,12 +136,22 @@ class CustomSimDataset(Dataset):
             bin_mask = self.load_mask(path)
             if self.do_erode_mask:
                 bin_mask = mask_erode(bin_mask, kernel_size=11)
-        else:
-            bin_mask = None
+            sample["mask"] = bin_mask
 
-        sample = get_ds_sample(
-            color, depth_m=depth_raw, rgb_path=path, mask=bin_mask, intrinsics=self.K, transforms=self.transforms
-        )
+        sample["rgb"] = color
+        sample["depth"] = depth_raw
+        sample["rgb_path"] = path
+        sample["pose"] = pose
+
+        sample["intrinsics"] = self.K
+
+        sample["mesh_pts"] = self.mesh_pts
+        sample["mesh_bbox"] = self.mesh_bbox
+        sample["mesh_diameter"] = self.mesh_diameter
+        sample["obj_name"] = self.obj_name
+        sample["priv"] = priv
+
+        sample = process_raw_sample(sample, transforms=self.transforms, convert_pose_to_quat=self.convert_pose_to_quat)
 
         return sample
 
@@ -171,13 +186,9 @@ class CustomSimDatasetEval(CustomSimDataset):
     def __len__(self):
         return min(len(self.color_files), len(self.pred_file_paths))
 
-    def get_pred_pose(self, i):
-        pose = load_pose(self.preds_path / f"{self.id_strs[i]}.txt")
-        return pose
-
     def __getitem__(self, i):
         sample = super().__getitem__(i)
-        sample["pose_pred"] = self.get_pred_pose(i)
+        sample["pose_pred"] = load_pose(f"{self.preds_path}/{self.id_strs[i]}.txt")
         if self.do_remap_pose_from_isaac:
             sample["pose_pred"] = self.pose_remap_from_isaac(sample["pose_pred"])
         return sample
