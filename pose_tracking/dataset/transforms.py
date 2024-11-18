acf0d9cf22e1bc57c33014168aecb2626dd0ed1e
kirilllzaitsev <kzaitsev88@gmail.com>
2024-11-18 23:24:35 +0100

    d:tweak mask_pixels

--- pose_tracking/dataset/transforms.py
+++ pose_tracking/dataset/transforms.py
@@ -13,10 +13,23 @@ def get_transforms(use_norm=False):
     return Compose(ts)
 
 
-def mask_pixels(img, p=0.5, pixels_masked_share_range=0.5):
-    us, vs = np.where(img > 0)
+def mask_pixels(img, p=0.5, pixels_masked_max_percent=0.1):
+    if len(img.shape) == 2:
+        us, vs = np.where(img > 0)
+        ch_axis = None
+    else:
+        if img.shape[0] in [1, 3]:
+            ch_axis = 0
+        else:
+            ch_axis = 2
+        us, vs = np.where(np.any(img > 0, axis=ch_axis))
     if np.random.uniform() < p:
-        pixels_masked_share = np.random.uniform(0, pixels_masked_share_range)
+        pixels_masked_share = np.random.uniform(0, pixels_masked_max_percent)
         pxs = np.random.choice(np.arange(0, len(us)), int(pixels_masked_share * len(us)), replace=False)
-        img[vs[pxs], us[pxs]] = 0
+        if ch_axis is None:
+            img[us[pxs], vs[pxs]] = 0
+        elif ch_axis == 0:
+            img[:, us[pxs], vs[pxs]] = 0
+        else:
+            img[us[pxs], vs[pxs], :] = 0
     return img
