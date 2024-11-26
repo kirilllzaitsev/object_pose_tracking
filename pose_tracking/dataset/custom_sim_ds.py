3db39f85229b7fed23c631b3a298e3e40908c6fa
kirilllzaitsev <kzaitsev88@gmail.com>
2024-11-26 23:01:48 +0100

    d:CustomSimDataset->TrackingD

--- pose_tracking/dataset/custom_sim_ds.py
+++ pose_tracking/dataset/custom_sim_ds.py
@@ -12,210 +12,94 @@
     └── *.png
 """
 
-import copy
-import glob
 import json
 import os
 from pathlib import Path
 
-import cv2
-import numpy as np
 import torch
-import trimesh
 from pose_tracking.config import logger
-from pose_tracking.dataset.ds_common import process_raw_sample
+from pose_tracking.dataset.tracking_ds import TrackingDataset
 from pose_tracking.utils.geom import (
     backproj_depth,
     bbox_to_8_point_centered,
     get_inv_pose,
 )
-from pose_tracking.utils.io import load_color, load_depth, load_mask, load_pose
+from pose_tracking.utils.io import load_pose
 from pose_tracking.utils.pcl import (
     downsample_pcl_via_subsampling,
     downsample_pcl_via_voxels,
 )
 from pose_tracking.utils.rotation_conversions import quaternion_to_matrix
-from pose_tracking.utils.segm_utils import mask_erode
-from pose_tracking.utils.trimesh_utils import compute_pts_span, load_mesh
-from torch.utils.data import Dataset
+from pose_tracking.utils.trimesh_utils import compute_pts_span
 
 
-class CustomSimDataset(Dataset):
+class CustomSimDataset(TrackingDataset):
 
     ds_name = "custom_sim"
 
     def __init__(
         self,
-        root_dir,
+        *args,
         cam_init_rot=None,
-        zfar=np.inf,
-        transforms=None,
         mesh_path=None,
         cam_pose_path=None,
-        obj_name="cube",
         obj_id=None,
-        num_mesh_pts=2000,
-        do_convert_pose_to_quat=False,
         do_remap_pose_from_isaac=True,
-        do_erode_mask=False,
         do_load_bbox_from_metadata=False,
-        include_masks=True,
         use_priv_info=False,
+        **kwargs,
     ):
-        self.include_masks = include_masks
+
         self.do_remap_pose_from_isaac = do_remap_pose_from_isaac
-        self.do_erode_mask = do_erode_mask
         self.use_priv_info = use_priv_info
-        self.convert_pose_to_quat = do_convert_pose_to_quat
         self.do_load_bbox_from_metadata = do_load_bbox_from_metadata
 
-        self.root_dir = root_dir
-        self.transforms = transforms
-        self.obj_name = obj_name
         self.obj_id = obj_id
-        self.zfar = zfar
+        self.cam_pose_path = cam_pose_path
 
-        self.color_files = sorted(glob.glob(f"{self.root_dir}/rgb/*.png"))
-        self.K = np.loadtxt(f"{self.root_dir}/intrinsics.txt").reshape(3, 3)
-        self.id_strs = []
-        for color_file in self.color_files:
-            id_str = os.path.basename(color_file).replace(".png", "")
-            self.id_strs.append(id_str)
-        self.H, self.W = cv2.imread(self.color_files[0]).shape[:2]
-        self.init_mask = self.load_mask(self.color_files[0])
+        super().__init__(*args, **kwargs)
+
         if mesh_path is None:
-            for name in ["mesh", obj_name]:
-                mesh_path_prop = f"{root_dir}/mesh/{obj_name}/{name}.obj"
+            for name in ["mesh", self.obj_name]:
+                mesh_path_prop = f"{self.video_dir}/mesh/{self.obj_name}/{name}.obj"
                 if os.path.exists(mesh_path_prop):
                     mesh_path = mesh_path_prop
                     break
-        self.mesh_path = mesh_path
-
-        if self.use_priv_info:
-            self.ds_no_occ = CustomSimDataset(
-                root_dir=Path(root_dir).parent / "custom_sim_textured_no_occlusion",
-                include_masks=True,
-                zfar=zfar,
-                transforms=transforms,
-                mesh_path=mesh_path,
-                cam_pose_path=cam_pose_path,
-                do_remap_pose_from_isaac=do_remap_pose_from_isaac,
-                do_erode_mask=do_erode_mask,
-                use_priv_info=False,
-            )
+        if mesh_path is not None:
+            self.set_up_obj_mesh(mesh_path)
 
         if cam_pose_path is None:
-            if os.path.exists(f"{self.root_dir}/cam_pose.txt"):
-                cam_pose_path = f"{self.root_dir}/cam_pose.txt"
+            if os.path.exists(f"{self.video_dir}/cam_pose.txt"):
+                cam_pose_path = f"{self.video_dir}/cam_pose.txt"
 
         if cam_pose_path is not None:
             assert cam_init_rot is not None
-            cam_pose_path = Path(cam_pose_path)
+            self.cam_pose_path = Path(cam_pose_path)
             self.cam_pose = load_pose(cam_pose_path)
             self.cam_init_rot = quaternion_to_matrix(torch.tensor(cam_init_rot)).numpy()
             self.cam_pose[:3, :3] = self.cam_pose[:3, :3] @ self.cam_init_rot
             self.w2c = get_inv_pose(pose=self.cam_pose)
-
         else:
             self.cam_pose = None
 
-        if obj_id is not None:
-            self.objs_metadata = json.load(open(f"{root_dir}/metadata.json"))
+    def get_pose_paths(self):
+        paths = []
+        for idx, path in enumerate(self.color_files):
+            path = Path(path.replace("rgb/", "pose/")).parent
+            if self.obj_id is None:
+                path = path / self.obj_name / f"{self.id_strs[idx]}.txt"
+            else:
+                path = path / f"{self.id_strs[idx]}.npy"
+            paths.append(path)
+        return paths
 
-        if mesh_path is None:
-            self.mesh = None
-        else:
-            load_res = load_mesh(mesh_path)
-            self.mesh = load_res["mesh"]
-            self.mesh_bbox = copy.deepcopy(np.asarray(load_res["bbox"]))
-            self.mesh_diameter = load_res["diameter"]
-            self.mesh_pts = torch.tensor(trimesh.sample.sample_surface(self.mesh, num_mesh_pts)[0]).float()
-
-        metadata_path = f"{root_dir}/metadata.json"
-        if os.path.exists(metadata_path):
-            self.metadata = json.load(open(metadata_path))
-        else:
-            self.metadata = None
-        if do_load_bbox_from_metadata:
-            assert self.metadata is not None, f"metadata not found at {metadata_path}"
-            assert len(self.metadata) == 1, len(self.metadata)
-            self.mesh_bbox = bbox_to_8_point_centered(bbox=self.metadata[0]["bbox"])
-            self.mesh_diameter = compute_pts_span(self.mesh_bbox)
-
-    def __len__(self):
-        return len(self.color_files)
-
-    def __getitem__(self, idx):
-        path = self.color_files[idx]
-        color = self.get_color(idx)
-        depth_raw = self.get_depth(idx)
-        if self.obj_id is None:
-            pose = self.load_pose(
-                Path(self.color_files[idx].replace("rgb/", "pose/")).parent / self.obj_name / f"{self.id_strs[idx]}.txt"
-            )
-        else:
-            poses = self.load_pose(
-                Path(self.color_files[idx].replace("rgb/", "pose/")).parent / f"{self.id_strs[idx]}.npy"
-            )
-            pose = poses[self.obj_id]
-
-        priv = None
-        if self.use_priv_info:
-            sample_no_occ = self.ds_no_occ[idx]
-            depth_no_occ = sample_no_occ["depth"]
-            mask = sample_no_occ["mask"]
-            obj_depth = (depth_no_occ).squeeze().numpy()
-            obj_depth[mask == 0] = 0
-            obj_depth_3d, _ = backproj_depth(obj_depth, intrinsics=self.K)
-            obj_depth_3d = downsample_pcl_via_voxels(obj_depth_3d, voxel_size=0.01)
-            priv = downsample_pcl_via_subsampling(obj_depth_3d, num_pts=256)
-
-        sample = {}
-
-        if self.include_masks:
-            bin_mask = self.load_mask(path)
-            if self.do_erode_mask:
-                bin_mask = mask_erode(bin_mask, kernel_size=11)
-            sample["mask"] = bin_mask
-
-        sample["rgb"] = color
-        sample["depth"] = depth_raw
-        sample["rgb_path"] = path
-        sample["pose"] = pose
-
-        sample["intrinsics"] = self.K
-
-        if self.mesh is not None:
-            sample["mesh_pts"] = self.mesh_pts
-            sample["mesh_bbox"] = self.mesh_bbox
-            sample["mesh_diameter"] = self.mesh_diameter
-        sample["obj_name"] = self.obj_name
-        sample["priv"] = priv
-
-        sample = process_raw_sample(
-            sample, transforms_rgb=self.transforms, convert_pose_to_quat=self.convert_pose_to_quat
-        )
-
-        return sample
-
-    def load_pose(self, path):
-        pose = load_pose(path)
+    def get_pose(self, idx):
+        pose = load_pose(self.pose_files[idx])
+        if self.obj_id is not None:
+            pose = pose[self.obj_id]
         if self.do_remap_pose_from_isaac:
             pose = self.pose_remap_from_isaac(pose)
         return pose
-
-    def load_mask(self, path):
-        mask = load_mask(path.replace("rgb/", "masks/"))
-        mask = torch.from_numpy(mask).float()
-        return mask
-
-    def get_color(self, i):
-        color = load_color(self.color_files[i], wh=(self.W, self.H))
-        return color
-
-    def get_depth(self, i):
-        depth = load_depth(self.color_files[i].replace("rgb/", "depth/"), wh=(self.W, self.H), zfar=self.zfar)
-        return depth
 
     def pose_remap_from_isaac(self, pose):
         rt = self.w2c @ pose
@@ -225,8 +109,44 @@ class CustomSimDataset(Dataset):
 class CustomSimDatasetCube(CustomSimDataset):
     ds_name = "custom_sim_cube"
 
-    def __init__(self, obj_name="cube", cam_init_rot=(0.5, -0.5, 0.5, -0.5), *args, **kwargs):
-        super().__init__(obj_name=obj_name, cam_init_rot=cam_init_rot, *args, **kwargs)
+    def __init__(
+        self, obj_name="cube", cam_init_rot=(0.5, -0.5, 0.5, -0.5), do_remap_pose_from_isaac=False, *args, **kwargs
+    ):
+        super().__init__(
+            do_remap_pose_from_isaac=do_remap_pose_from_isaac,
+            obj_name=obj_name,
+            cam_init_rot=cam_init_rot,
+            *args,
+            **kwargs,
+        )
+
+        if self.use_priv_info:
+            video_dir = Path(self.video_dir).parent / "custom_sim_textured_no_occlusion"
+            for arg in ["use_priv_info", "video_dir", "include_mask", "do_remap_pose_from_isaac"]:
+                if arg in kwargs:
+                    kwargs.pop(arg)
+            self.ds_no_occ = CustomSimDatasetCube(
+                do_remap_pose_from_isaac=do_remap_pose_from_isaac,
+                include_mask=True,
+                video_dir=video_dir,
+                cam_init_rot=cam_init_rot,
+                obj_name=obj_name,
+                *args,
+                **kwargs,
+            )
+
+    def augment_sample(self, sample, idx):
+        if self.use_priv_info:
+            sample_no_occ = self.ds_no_occ[idx]
+            depth_no_occ = sample_no_occ["depth"]
+            mask = sample_no_occ["mask"]
+            obj_depth = (depth_no_occ).squeeze().numpy()
+            obj_depth[mask == 0] = 0
+            obj_depth_3d, _ = backproj_depth(obj_depth, intrinsics=self.K)
+            obj_depth_3d = downsample_pcl_via_voxels(obj_depth_3d, voxel_size=0.01)
+            priv = downsample_pcl_via_subsampling(obj_depth_3d, num_pts=256)
+            sample["priv"] = priv
+        return sample
 
 
 class CustomSimDatasetIkea(CustomSimDataset):
@@ -250,6 +170,20 @@ class CustomSimDatasetIkea(CustomSimDataset):
             **kwargs,
         )
 
+        if obj_id is not None:
+            self.objs_metadata = json.load(open(f"{self.video_dir}/metadata.json"))
+
+        metadata_path = f"{self.video_dir}/metadata.json"
+        if os.path.exists(metadata_path):
+            self.metadata = json.load(open(metadata_path))
+        else:
+            self.metadata = None
+        if do_load_bbox_from_metadata:
+            assert self.metadata is not None, f"metadata not found at {metadata_path}"
+            assert len(self.metadata) == 1, len(self.metadata)
+            self.mesh_bbox = bbox_to_8_point_centered(bbox=self.metadata[0]["bbox"])
+            self.mesh_diameter = compute_pts_span(self.mesh_bbox)
+
 
 class CustomSimDatasetEval(CustomSimDataset):
     def __init__(self, preds_path, *args, **kwargs):
@@ -264,7 +198,7 @@ class CustomSimDatasetEval(CustomSimDataset):
     def __len__(self):
         return min(len(self.color_files), len(self.pred_file_paths))
 
-    def __getitem__(self, i):
-        sample = super().__getitem__(i)
-        sample["pose_pred"] = self.load_pose(f"{self.preds_path}/{self.id_strs[i]}.txt")
+    def augment_sample(self, sample, idx):
+        sample = super().augment_sample(sample, idx)
+        sample["pose_pred"] = self.get_pose(f"{self.preds_path}/{self.id_strs[idx]}.txt")
         return sample
