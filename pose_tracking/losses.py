a3f63385b8bc45120a0c9ebbf351a0d88d2db8b2
kirilllzaitsev <kzaitsev88@gmail.com>
2024-11-30 23:54:17 +0100

    u:add dice_loss

--- pose_tracking/losses.py
+++ pose_tracking/losses.py
@@ -107,6 +107,24 @@ def kpt_cross_ratio_loss(kpts_pred, bbox_2d_kpts_collinear_idxs, cr=4 / 3):
     return lcr
 
 
+def dice_loss(inputs, targets, num_boxes):
+    """
+    Compute the DICE loss, similar to generalized IOU for masks
+    Args:
+        inputs: A float tensor of arbitrary shape.
+                The predictions for each example.
+        targets: A float tensor with the same shape as inputs. Stores the binary
+                 classification label for each element in inputs
+                (0 for the negative class and 1 for the positive class).
+    """
+    inputs = inputs.sigmoid()
+    inputs = inputs.flatten(1)
+    numerator = 2 * (inputs * targets).sum(1)
+    denominator = inputs.sum(-1) + targets.sum(-1)
+    loss = 1 - (numerator + 1) / (denominator + 1)
+    return loss.sum() / num_boxes
+
+
 def get_t_loss(t_loss_name):
     if t_loss_name == "mse":
         criterion_trans = nn.MSELoss()
