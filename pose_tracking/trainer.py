18ad52323dc764653ea7c1d52b704fb41b7d5994
kirilllzaitsev <kzaitsev88@gmail.com>
2024-12-02 11:09:25 +0100

    t:2dt with norm depth

--- pose_tracking/trainer.py
+++ pose_tracking/trainer.py
@@ -307,9 +307,13 @@ class Trainer:
                     t_gt_2d_norm[:, 1] = t_gt_2d_norm[:, 1] / h
 
                     t_pred_2d = out["t"]
-                    loss_t_2d = torch.abs(t_pred_2d - t_gt_2d_norm).mean()
-                    loss_center_depth = torch.abs(center_depth_pred - depth_gt).mean()
 
+                    max_depth = 10
+                    depth_gt /= max_depth
+                    center_depth_pred = torch.sigmoid(center_depth_pred)
+
+                    loss_t_2d = self.criterion_trans(t_pred_2d, t_gt_2d_norm)
+                    loss_center_depth = torch.abs(center_depth_pred - depth_gt).mean()
                     loss_t = loss_t_2d + loss_center_depth
                 else:
                     if self.do_predict_rel_pose:
@@ -859,7 +863,81 @@ class TrainerVideopose(Trainer):
         preds_dir=None,
         stage="train",
     ):
+        from pizza.lib.losses.rotation import measure_rotation
+        from pizza.lib.losses.translation import measure_translation
+
         if stage == "train":
             self.train_epoch_count += 1
         running_stats = defaultdict(float)
+
+        # each batched seq is processed at once
+        is_train = optimizer is not None
+        do_opt_every_ts = is_train and self.use_optim_every_ts
+        do_opt_in_the_end = is_train and not self.use_optim_every_ts
+        do_vis = False
+        seq_pbar = tqdm(loader, desc="Seq", leave=False)
+        MSELoss = torch.nn.MSELoss()
+
+        for seq_pack_idx, batched_seq in enumerate(seq_pbar):
+            seq_size = len(batched_seq["rgb"])
+            batched_seq = transfer_batch_to_device(batched_seq, self.device)
+            res = self.model(batched_seq)
+            rot = res["rot"]
+            delta_uv = res["delta_uv"]
+            delta_depth = res["delta_depth"]
+
+            batch = batched_seq
+            ratio = batch["ratio"]
+            uv_first_frame = batch["uv_first_frame"]
+            gt_delta_uv = batch["delta_uv"]
+            gt_delta_depth = batch["delta_depth"]
+            depth_first_frame = batch["depth_first_frame"]
+            gt_delta_rotation = batch["gt_delta_rotation"]
+            rotation_first_frame = batch["rotation_first_frame"]
+            gt_rotations = batch["gt_rotations"]
+
+            T_metrics = measure_translation(
+                delta_uv_pred=delta_uv,
+                delta_d_pred=delta_depth,
+                uv_first_frame=uv_first_frame,
+                d_first_frame=depth_first_frame,
+                gt_delta_uv=gt_delta_uv,
+                gt_delta_d=gt_delta_depth,
+                alpha_resize=ratio,
+                cumulative=True,
+                loss_function=torch.nn.MSELoss(reduction="sum").cuda(depth_first_frame.get_device()),
+                dataset_name="test",
+            )
+            R_metrics = measure_rotation(
+                axis_angles_pred=rot,
+                axis_angles_gt=gt_delta_rotation,
+                rot_first_frame=rotation_first_frame,
+                rots_gt=gt_rotations,
+                cumulative=True,
+            )
+
+            loss_Z = MSELoss(delta_depth, gt_delta_depth) * (10**3)  # to mm
+            loss_UV = MSELoss(delta_uv, gt_delta_uv) * (10**3)  # to mm
+            loss = loss_Z + loss_UV + R_metrics["geodesic_err"]
+
+            if is_train:
+                optimizer.zero_grad()
+                loss.backward()
+                # Call step of optimizer to update model params
+                optimizer.step()
+
+            losses = {
+                "loss": loss,
+                "loss_Z": loss_Z,
+                "loss_UV": loss_UV,
+            }
+            for k, v in {**T_metrics, **R_metrics, **losses}.items():
+                if isinstance(v, torch.Tensor):
+                    v = v.item()
+                    if self.use_ddp:
+                        reduce_metric(v, world_size=self.world_size)
+                running_stats[k] += v
+                if self.do_log:
+                    self.writer.add_scalar(f"{stage}_seq/{k}", v, self.seq_counts_per_stage[stage])
+
         return running_stats
