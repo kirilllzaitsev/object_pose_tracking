f3729159827811c637f2fe10bfefc7e18c2a6d74
kirilllzaitsev <kzaitsev88@gmail.com>
2024-12-08 20:56:06 +0100

    m:add efficientnet_b0

--- pose_tracking/models/encoders.py
+++ pose_tracking/models/encoders.py
@@ -18,7 +18,13 @@ def get_encoders(
     else:
         norm_layer = nn.Identity
 
-    assert model_name in ["regnet_y_800mf", "efficientnet_b1", "efficientnet_v2_s", "mobilenet_v3_small"], model_name
+    assert model_name in [
+        "regnet_y_800mf",
+        "efficientnet_b0",
+        "efficientnet_b1",
+        "efficientnet_v2_s",
+        "mobilenet_v3_small",
+    ], model_name
 
     if model_name in ["regnet_y_800mf", "efficientnet_v2_s"]:
         print(f"WARN: {model_name} should not be used with CNNLSTM because of the batchnorm")
@@ -31,6 +37,19 @@ def get_encoders(
         for m in [encoder_rgb, encoder_depth]:
             m.fc = nn.Sequential(
                 nn.Linear(784, out_dim),
+            )
+    elif model_name == "efficientnet_b0":
+        weights = torchvision.models.EfficientNet_B0_Weights.DEFAULT
+        encoder_rgb = torchvision.models.efficientnet_b0(
+            weights=weights if weights_rgb == "imagenet" else None, norm_layer=norm_layer
+        )
+        encoder_depth = torchvision.models.efficientnet_b0(
+            weights=weights if weights_depth == "imagenet" else None, norm_layer=norm_layer
+        )
+        encoder_depth.features[0][0] = nn.Conv2d(1, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
+        for m in [encoder_rgb, encoder_depth]:
+            m.classifier = nn.Sequential(
+                nn.Linear(1280, out_dim),
             )
     elif model_name == "efficientnet_b1":
         weights = torchvision.models.EfficientNet_B1_Weights.IMAGENET1K_V2
