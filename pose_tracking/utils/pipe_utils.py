8fed5a40a4a62ffdb7d0d7fe51d8ce6d807001e3
kirilllzaitsev <kzaitsev88@gmail.com>
2025-02-02 12:37:45 +0100

    u:do not zero out false neg/pos aug for tf

--- pose_tracking/utils/pipe_utils.py
+++ pose_tracking/utils/pipe_utils.py
@@ -257,8 +257,8 @@ def get_trackformer_args(args):
     tf_args.overflow_boxes = True
     tf_args.multi_frame_encoding = args.tf_use_multi_frame_encoding
 
-    tf_args.track_query_false_negative_prob = 0.0
-    tf_args.track_query_false_positive_prob = 0.0
+    tf_args.track_query_false_negative_prob = args.tf_track_query_false_negative_prob
+    tf_args.track_query_false_positive_prob = args.tf_track_query_false_positive_prob
 
     tf_args.lr = args.lr
     tf_args.lr_backbone = args.lr_encoders
