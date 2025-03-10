35136d6c44d44dfd07a0115705fcc0d8f420c659
kirilllzaitsev <kzaitsev88@gmail.com>
2025-03-10 21:45:48 +0100

    u:add allo-ego u

--- pose_tracking/utils/geom.py
+++ pose_tracking/utils/geom.py
@@ -1,10 +1,16 @@
+import math
+
 import cv2
 import numpy as np
+from pose_tracking.utils.pose import matrix_to_quaternion
+from pose_tracking.utils.rotation_conversions import quaternion_to_matrix
 import torch
 from pose_tracking.config import logger
 from pose_tracking.utils.common import cast_to_numpy
 from pose_tracking.utils.misc import is_tensor, pick_library
 from scipy.spatial.transform import Rotation as R
+from transforms3d.axangles import axangle2mat
+from transforms3d.quaternions import axangle2quat, mat2quat, qmult, quat2mat
 
 
 def world_to_2d(pts, K, rt):
@@ -683,3 +689,195 @@ def compute_normals(depth):
 
     surface_normals = cv2.merge([N1, N2, N3])
     return surface_normals
+
+
+def allocentric_to_egocentric(allo_pose, src_type="mat", dst_type="mat", cam_ray=(0, 0, 1.0)):
+    """https://github.com/THU-DA-6D-Pose-Group/GDR-Net/blob/main/core/gdrn_modeling/models/GDRN.py"""
+    # Compute rotation between ray to object centroid and optical center ray
+    cam_ray = np.asarray(cam_ray)
+    if src_type == "mat":
+        trans = allo_pose[:3, 3]
+    elif src_type == "quat":
+        trans = allo_pose[4:7]
+    else:
+        raise ValueError("src_type should be mat or quat, got: {}".format(src_type))
+    obj_ray = trans.copy() / np.linalg.norm(trans)
+    angle = math.acos(cam_ray.dot(obj_ray))
+
+    # Rotate back by that amount
+
+    if angle > 0:
+        if dst_type == "mat":
+            ego_pose = np.zeros((3, 4), dtype=allo_pose.dtype)
+            ego_pose[:3, 3] = trans
+            rot_mat = axangle2mat(axis=np.cross(cam_ray, obj_ray), angle=angle)
+            if src_type == "mat":
+                ego_pose[:3, :3] = np.dot(rot_mat, allo_pose[:3, :3])
+            elif src_type == "quat":
+                ego_pose[:3, :3] = np.dot(rot_mat, quat2mat(allo_pose[:4]))
+        elif dst_type == "quat":
+            ego_pose = np.zeros((7,), dtype=allo_pose.dtype)
+            ego_pose[4:7] = trans
+            rot_q = axangle2quat(np.cross(cam_ray, obj_ray), angle)
+            if src_type == "quat":
+                ego_pose[:4] = qmult(rot_q, allo_pose[:4])
+            elif src_type == "mat":
+                ego_pose[:4] = qmult(rot_q, mat2quat(allo_pose[:3, :3]))
+        else:
+            raise ValueError("dst_type should be mat or quat, got: {}".format(dst_type))
+    else:  # allo to ego
+        if src_type == "mat" and dst_type == "quat":
+            ego_pose = np.zeros((7,), dtype=allo_pose.dtype)
+            ego_pose[:4] = mat2quat(allo_pose[:3, :3])
+            ego_pose[4:7] = allo_pose[:3, 3]
+        elif src_type == "quat" and dst_type == "mat":
+            ego_pose = np.zeros((3, 4), dtype=allo_pose.dtype)
+            ego_pose[:3, :3] = quat2mat(allo_pose[:4])
+            ego_pose[:3, 3] = allo_pose[4:7]
+        else:
+            ego_pose = allo_pose.copy()
+    return ego_pose
+
+
+def egocentric_to_allocentric(ego_pose, src_type="mat", dst_type="mat", cam_ray=(0, 0, 1.0)):
+    # Compute rotation between ray to object centroid and optical center ray
+    cam_ray = np.asarray(cam_ray)
+    if src_type == "mat":
+        trans = ego_pose[:3, 3]
+    elif src_type == "quat":
+        trans = ego_pose[4:7]
+    else:
+        raise ValueError("src_type should be mat or quat, got: {}".format(src_type))
+    obj_ray = trans.copy() / np.linalg.norm(trans)
+    angle = math.acos(cam_ray.dot(obj_ray))
+
+    # Rotate back by that amount
+    if angle > 0:
+        if dst_type == "mat":
+            allo_pose = np.zeros((4, 4), dtype=ego_pose.dtype)
+            allo_pose[:3, 3] = trans
+            rot_mat = axangle2mat(axis=np.cross(cam_ray, obj_ray), angle=-angle)
+            if src_type == "mat":
+                allo_pose[:3, :3] = np.dot(rot_mat, ego_pose[:3, :3])
+            elif src_type == "quat":
+                allo_pose[:3, :3] = np.dot(rot_mat, quat2mat(ego_pose[:4]))
+        elif dst_type == "quat":
+            allo_pose = np.zeros((7,), dtype=ego_pose.dtype)
+            allo_pose[4:7] = trans
+            rot_q = axangle2quat(np.cross(cam_ray, obj_ray), -angle)
+            if src_type == "quat":
+                allo_pose[:4] = qmult(rot_q, ego_pose[:4])
+            elif src_type == "mat":
+                allo_pose[:4] = qmult(rot_q, mat2quat(ego_pose[:3, :3]))
+        else:
+            raise ValueError("dst_type should be mat or quat, got: {}".format(dst_type))
+    else:
+        if src_type == "mat" and dst_type == "quat":
+            allo_pose = np.zeros((7,), dtype=ego_pose.dtype)
+            allo_pose[:4] = mat2quat(ego_pose[:3, :3])
+            allo_pose[4:7] = ego_pose[:3, 3]
+        elif src_type == "quat" and dst_type == "mat":
+            allo_pose = np.zeros((4, 4), dtype=ego_pose.dtype)
+            allo_pose[:3, :3] = quat2mat(ego_pose[:4])
+            allo_pose[:3, 3] = ego_pose[4:7]
+        else:
+            allo_pose = ego_pose.copy()
+    if src_type == "mat":
+        allo_pose[3, 3] = 1
+    return allo_pose
+
+
+def quatmul_torch(q1, q2):
+    """Computes the multiplication of two quaternions.
+
+    Note, output dims: NxMx4 with N being the batchsize and N the number
+    of quaternions or 3D points to be transformed.
+    """
+    # RoI dimension. Unsqueeze if not fitting.
+    a = q1.unsqueeze(0) if q1.dim() == 1 else q1
+    b = q2.unsqueeze(0) if q2.dim() == 1 else q2
+
+    # Corner dimension. Unsequeeze if not fitting.
+    a = a.unsqueeze(1) if a.dim() == 2 else a
+    b = b.unsqueeze(1) if b.dim() == 2 else b
+
+    # Quaternion product
+    x = a[:, :, 1] * b[:, :, 0] + a[:, :, 2] * b[:, :, 3] - a[:, :, 3] * b[:, :, 2] + a[:, :, 0] * b[:, :, 1]
+    y = -a[:, :, 1] * b[:, :, 3] + a[:, :, 2] * b[:, :, 0] + a[:, :, 3] * b[:, :, 1] + a[:, :, 0] * b[:, :, 2]
+    z = a[:, :, 1] * b[:, :, 2] - a[:, :, 2] * b[:, :, 1] + a[:, :, 3] * b[:, :, 0] + a[:, :, 0] * b[:, :, 3]
+    w = -a[:, :, 1] * b[:, :, 1] - a[:, :, 2] * b[:, :, 2] - a[:, :, 3] * b[:, :, 3] + a[:, :, 0] * b[:, :, 0]
+
+    return torch.stack((w, x, y, z), dim=2)
+
+
+def allocentric_to_egocentric_torch(translation, q_allo, eps=1e-4):
+    """Given an allocentric (object-centric) pose, compute new camera-centric
+    pose Since we do detection on the image plane and our kernels are
+    2D-translationally invariant, we need to ensure that rendered objects
+    always look identical, independent of where we render them.
+
+    Since objects further away from the optical center undergo skewing, we try to visually correct by
+    rotating back the amount between optical center ray and object centroid ray.
+    Another way to solve that might be translational variance (https://arxiv.org/abs/1807.03247)
+    Args:
+        translation: Nx3
+        q_allo: Nx4
+    """
+
+    # Compute rotation between ray to object centroid and optical center ray
+    cam_ray = torch.tensor([0, 0, 1.0], dtype=translation.dtype, device=translation.device)  # (3,)
+    obj_ray = translation / (torch.norm(translation, dim=1, keepdim=True) + eps)
+
+    # cam_ray.dot(obj_ray), assume cam_ray: (0, 0, 1)
+    angle = obj_ray[:, 2:3].acos()
+
+    # Compute rotation between ray to object centroid and optical center ray
+    axis = torch.cross(cam_ray.expand_as(obj_ray), obj_ray)
+    axis = axis / (torch.norm(axis, dim=1, keepdim=True) + eps)
+
+    # Build quaternion representing the rotation around the computed axis
+    # angle-axis => quat
+    q_allo_to_ego = torch.cat(
+        [
+            torch.cos(angle / 2.0),
+            axis[:, 0:1] * torch.sin(angle / 2.0),
+            axis[:, 1:2] * torch.sin(angle / 2.0),
+            axis[:, 2:3] * torch.sin(angle / 2.0),
+        ],
+        dim=1,
+    )
+
+    # Apply quaternion for transformation from allocentric to egocentric.
+    q_ego = quatmul_torch(q_allo_to_ego, q_allo)[:, 0]  # Remove added Corner dimension here.
+    return q_ego
+
+
+def allo_to_ego_mat_torch(translation, rot_allo, eps=1e-4):
+    # translation: Nx3
+    # rot_allo: Nx3x3
+    # Compute rotation between ray to object centroid and optical center ray
+    cam_ray = torch.tensor([0, 0, 1.0], dtype=translation.dtype, device=translation.device)  # (3,)
+    obj_ray = translation / (torch.norm(translation, dim=1, keepdim=True) + eps)
+
+    # cam_ray.dot(obj_ray), assume cam_ray: (0, 0, 1)
+    angle = obj_ray[:, 2:3].acos()
+
+    # Compute rotation between ray to object centroid and optical center ray
+    axis = torch.cross(cam_ray.expand_as(obj_ray), obj_ray)
+    axis = axis / (torch.norm(axis, dim=1, keepdim=True) + eps)
+
+    # Build quaternion representing the rotation around the computed axis
+    # angle-axis => quat
+    q_allo_to_ego = torch.cat(
+        [
+            torch.cos(angle / 2.0),
+            axis[:, 0:1] * torch.sin(angle / 2.0),
+            axis[:, 1:2] * torch.sin(angle / 2.0),
+            axis[:, 2:3] * torch.sin(angle / 2.0),
+        ],
+        dim=1,
+    )
+    rot_allo_to_ego = quaternion_to_matrix(q_allo_to_ego)
+    # Apply quaternion for transformation from allocentric to egocentric.
+    rot_ego = torch.matmul(rot_allo_to_ego, rot_allo)
+    return rot_ego
